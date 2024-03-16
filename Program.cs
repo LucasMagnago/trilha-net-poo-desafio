bea0c784764dbe015a4647cdea2ecbe6767ef995
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2024-03-16 14:20:58 -0300

    completed

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("--- NOKIA ---");
+Nokia smartphoneNokia = new Nokia("27998877665", "X100", "998877665544332211", 8);
+smartphoneNokia.ReceberLigacao();
+smartphoneNokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("--- IPHONE ---");
+Iphone smartphoneIphone = new Iphone("27112233445", "14 PRO MAX", "112233445566778899", 4);
+smartphoneIphone.ReceberLigacao();
+smartphoneIphone.InstalarAplicativo("WhatsApp");
